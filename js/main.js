0d1e6c95b3d21cc452060a23ab48cb88ebb13755
sb227 <69783498+sb227@users.noreply.github.com>
2021-02-02 17:25:37 +0900

    210202

--- js/main.js
+++ js/main.js
@@ -3,5 +3,20 @@
 $(function (e) { ///////////////// JQB //////////////////////
     console.log('실행');
 
+
+    $('.menu_btn').click(function () {
+		console.log('dd')
+		$('.mob_menu').addClass('active');
+		$('.mob_bg').addClass('active');
+		$('.close_btn').addClass('active');
+		$('.lnb_mob').addClass('active');
+		$('.gnb_mob').addClass('active');
+	}); ///////// click /////////////
+
+	$('.close_btn').click(function () {
+		$('.mob_menu').removeClass('active');
+		$('.lnb_mob').removeClass('active');
+		$('.gnb_mob').removeClass('active');
+	}); ///////// click ////////////
 }); ///////////////// JQB //////////////////////
 
